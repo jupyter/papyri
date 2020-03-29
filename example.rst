1d62b38ef018f4559d1f93683305dd082dc772b9
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-03-28 17:53:46 -0700

    restart

--- example.rst
+++ example.rst
@@ -1,1 +1,8 @@
 This is an example text, that I'll try to reformat. It should be relatively long to make sure I can deal with long lines.
+
+Here is a second line, or paragraph, not sure
+With some second lines. 
+
+   Here is some indented stuff
+
+And back to a third
--- minirst/__init__.py
+++ minirst/__init__.py
@@ -3,6 +3,47 @@
 
 __version__ = '0.0.1'
 
+import string
+import re
+
+
+def parse_document(indent_lines):
+    tree, rest = [], indent_lines
+
+
+def parse_paragraph(lines):
+    lines = list(lines)
+    l0, text = lines[0]
+
+    for ix,lx,tx in enumerate(lines[1:]):
+        if lx == l0:
+            text+=' '+tx
+        else:
+            break
+
+    return (text, lines[ix:])
+
+
+
+
+
+
+
+def split_indents(lines:list)-> list:
+    """
+    Split lines by indent level (number of leading spaces)
+    """
+    res = []
+    for l in lines:
+        trimmed = l.lstrip()
+        if not trimmed:
+            res.append((None,l))
+        else:
+            res.append((len(l)-len(trimmed),l))
+    return res
+    
+
+
 def reformat(original):
     temp = original.split(' ')
     
@@ -27,7 +68,9 @@ def main():
         original = f.read()
     print(original)
     print('-'*50)
-    print('||'+reformat(original)+'||')
+    for l in split_indents(original.splitlines()):
+        print(l)
+    #print('||'+reformat(original)+'||')
 
 
 if __name__ == '__main__':
