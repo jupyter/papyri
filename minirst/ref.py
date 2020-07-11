93fbbb5266fa9992d4a7b538f487e01f9ad999b9
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-07-10 20:54:02 -0700

    reformat

--- minirst/ref.py
+++ minirst/ref.py
@@ -1,153 +1,154 @@
-import numpy as np
-import difflib
-
-file = np.__file__
-file = '/Users/bussonniermatthias/dev/numpy/numpy/matlib.py'
-with open(file, 'r') as f:
-    data = f.read()
-    
 import ast
-tree = ast.parse(data)
-
-funcs = [t for t in tree.body if isinstance(t, ast.FunctionDef)]
-
+import sys
 import difflib
+from textwrap import indent
+
+import numpy as np
+
+import numpydoc.docscrape
+
+
 
 def w(orig):
     ll = []
     for l in orig:
-        if l[0] in '+-':
-            #ll.append(l.replace(' ', '⎵'))
-            ll.append(l.replace(' ', '·'))
-            
+        if l[0] in "+-":
+            # ll.append(l.replace(' ', '⎵'))
+            ll.append(l.replace(" ", "·"))
+
         else:
             ll.append(l)
     lll = []
     for l in ll:
-        if l.endswith('\n'):
+        if l.endswith("\n"):
             lll.append(l[:-1])
         else:
             lll.append(l[:])
     return lll
-import numpydoc.docscrape
-from textwrap import indent
+
 
 class DocstringFormatter:
-
     @classmethod
     def format_Summary(self, s):
-        return '\n'.join(s)+'\n'
+        return "\n".join(s) + "\n"
 
     @classmethod
     def format_Extended_Summary(self, es):
-        return '\n'.join(es)+'\n'
+        return "\n".join(es) + "\n"
 
     @classmethod
     def _format_ps(cls, name, ps):
-    
-        out  = name+'\n'
-        out += '-'*len(name)+'\n'
+
+        out = name + "\n"
+        out += "-" * len(name) + "\n"
         for p in ps:
             if p.type:
-                out+= f'''{p.name} : {p.type}\n'''
+                out += f"""{p.name} : {p.type}\n"""
             else:
-                out+= f'''{p.name}\n'''
-            out+= indent('\n'.join(p.desc), '    ')
-            out+= '\n'
-        return out   
-    
+                out += f"""{p.name}\n"""
+            out += indent("\n".join(p.desc), "    ")
+            out += "\n"
+        return out
+
     @classmethod
     def format_Parameters(cls, ps):
-        return cls._format_ps('Parameters', ps)
-        
-    
+        return cls._format_ps("Parameters", ps)
+
     @classmethod
     def format_Other_Parameters(cls, ps):
-        return cls._format_ps('Other Parameters', ps)
-    
+        return cls._format_ps("Other Parameters", ps)
+
     @classmethod
     def format_See_Also(cls, sas):
-        out  = 'See Also\n'
-        out  +='--------\n'
-        
-        for a,b in sas:
+        out = "See Also\n"
+        out += "--------\n"
+
+        for a, b in sas:
             if b:
-                out+='Some See also B !\n'
+                out += "Some See also B !\n"
             if len(a) > 1:
-                out+='Some See Long A !\n'
+                out += "Some See Long A !\n"
             ref, type_ = a[0]
             if type_ is not None:
-                out += f':{type_}:`{ref}`\n'
+                out += f":{type_}:`{ref}`\n"
             else:
-                out += f'{ref}\n'
+                out += f"{ref}\n"
         return out
-    
+
     @classmethod
     def format_Notes(cls, lines):
-        out  = 'Notes\n'
-        out += '-----\n'
-        out+= '\n'.join(lines)
-        out+= '\n'
-        return out   
-    
+        out = "Notes\n"
+        out += "-----\n"
+        out += "\n".join(lines)
+        out += "\n"
+        return out
+
     @classmethod
     def format_Examples(cls, lines):
-        out  = 'Examples\n'
-        out += '--------\n'
-        out+= '\n'.join(lines)
-        out+= '\n'
-        return out  
-        
+        out = "Examples\n"
+        out += "--------\n"
+        out += "\n".join(lines)
+        out += "\n"
+        return out
+
     @classmethod
     def format_Returns(self, ps):
-        out  = 'Returns\n'
-        out  += '-------\n'
+        out = "Returns\n"
+        out += "-------\n"
 
         for p in ps:
             if p.name:
-                out+= f'''{p.name} : {p.type}\n'''
+                out += f"""{p.name} : {p.type}\n"""
             else:
-                out+= f'''{p.type}\n'''
+                out += f"""{p.type}\n"""
             if p.desc:
-                out+= indent('\n'.join(p.desc), '    ')
-                out+= '\n'
-        return out    
-        
+                out += indent("\n".join(p.desc), "    ")
+                out += "\n"
+        return out
 
-        
+
 def test(docstr):
     if len(docstr.splitlines()) == 1:
         return
-    if not docstr.startswith('\n    '):
-        docstr = '\n    '+docstr
+    if not docstr.startswith("\n    "):
+        docstr = "\n    " + docstr
     doc = numpydoc.docscrape.NumpyDocString(docstr)
 
-
-    fmt = ''
+    fmt = ""
     for s in doc.sections:
         if doc[s]:
-            f = getattr(DocstringFormatter, 'format_'+s.replace(' ', '_'), lambda x:s)
-            fmt += '\n'
+            f = getattr(
+                DocstringFormatter, "format_" + s.replace(" ", "_"), lambda x: s
+            )
+            fmt += "\n"
             fmt += f(doc[s])
-    
-    fmt = indent(fmt, '    ')+'    '
-    #print(fmt)
+
+    fmt = indent(fmt, "    ") + "    "
+    # print(fmt)
 
     dold = docstr.splitlines()
     dnew = fmt.splitlines()
-    diffs = list(difflib.unified_diff(dold, dnew,n=100, fromfile='old',tofile='new'), )
+    diffs = list(difflib.unified_diff(dold, dnew, n=100, fromfile="old", tofile="new"),)
     if diffs:
-        
-        print(indent('\n'.join(w(diffs)), ' |   ', predicate=lambda x:True))
 
-for i,func in enumerate(funcs[:]):
-    print(i,'==', func.name, '==')
-    try:
-        docstring = func.body[0].value.s
-    except AttributeError:
-        continue
-    (func.body[0].lineno,
-    func.body[0].col_offset,
-    func.body[0].end_lineno)
+        print(indent("\n".join(w(diffs)), " |   ", predicate=lambda x: True))
 
-    test(docstring)
+
+def main():
+    file = sys.argv[1]
+    print(file)
+    with open(file, "r") as f:
+        data = f.read()
+
+    tree = ast.parse(data)
+
+    funcs = [t for t in tree.body if isinstance(t, ast.FunctionDef)]
+    for i, func in enumerate(funcs[:]):
+        print(i, "==", func.name, "==")
+        try:
+            docstring = func.body[0].value.s
+        except AttributeError:
+            continue
+        (func.body[0].lineno, func.body[0].col_offset, func.body[0].end_lineno)
+
+        test(docstring)
