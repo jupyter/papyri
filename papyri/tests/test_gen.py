05596d001644d3a8bb30bea8be2e015d6c143e07
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-08-11 20:24:21 -0700

    update tests

--- papyri/tests/test_gen.py
+++ papyri/tests/test_gen.py
@@ -8,6 +8,7 @@ from ..render import _ascii_render, _route
 
 def test_gen_numpy():
     with TemporaryDirectory() as t:
+        NFUNC = 25
         t = Path(t)
         g = Gen()
         g.cache_dir = t / "cache"
@@ -16,7 +17,7 @@ def test_gen_numpy():
         import time
 
         num = [x.name[:-5] for x in (t / "cache").glob("papyri/*.json")]
-        assert len(num) == 25
+        assert len(num) == NFUNC + 1
         assert "papyri.gen.gen_main" in num
 
         ing = Ingester()
@@ -25,7 +26,7 @@ def test_gen_numpy():
         ing.ingest_dir.mkdir()
         ing.ingest("papyri", check=True)
         ing_r = [x.name[:-5] for x in (ing.ingest_dir).glob("*.json")]
-        assert len(ing_r) == 24, f"{set(ing_r) - set(num)} | {set(num) - set(ing_r)}"
+        assert len(ing_r) == NFUNC, f"{set(ing_r) - set(num)} | {set(num) - set(ing_r)}"
 
         res = _route("papyri.gen.gen_main", ing.ingest_dir)
         assert "main entry point" in res
