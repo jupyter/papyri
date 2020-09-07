aa6215f4b48beddf15a061827b1c79ce9da28680
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-09-07 16:30:50 -0700

    isort and black

--- papyri/render.py
+++ papyri/render.py
@@ -1,146 +1,16 @@
 import json
 import os
 from collections import defaultdict
-from functools import lru_cache
 from pathlib import Path
-from types import ModuleType
 
-import requests
 from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
 from quart_trio import QuartTrio
 
-from .config import base_dir, html_dir, ingest_dir
-from .crosslink import SeeAlsoItem, load_one, resolve_
+from .config import html_dir, ingest_dir
+from .crosslink import load_one, resolve_
+from .stores import BaseStore, GHStore, Store
 from .take2 import Lines, Paragraph, make_block_3
 from .utils import progress
-
-PAT = os.environ.get("PAT", None)
-
-
-# maybe from cachetools import TTLCache
-
-
-class BaseStore:
-    def __init__(self, path):
-        if not isinstance(path, Path):
-            path = Path(path)
-        assert isinstance(path, Path)
-        self.path = path
-
-    def _other(self):
-        return type(self)
-
-    def __truediv__(self, other):
-        return self._other()(self.path / other)
-
-    def __str__(self):
-        return str(self.path)
-
-    async def exists(self):
-        return self.path.exists()
-
-    async def read_text(self):
-        return self.path.read_text()
-
-    def glob(self, arg):
-        return [self._other()(x) for x in self.path.glob(arg)]
-
-    @property
-    def name(self):
-        return self.path.name
-
-    def __lt__(self, other):
-        return self.path < other.path
-
-    def __le__(self, other):
-        return self.path <= other.path
-
-    def __eq__(self, other):
-        return self.path == other.path
-
-
-import re
-
-
-def gre(pat):
-    return re.compile(pat.replace(".", "\.").replace("*", ".+"))
-
-
-header = f"token {PAT}"
-
-
-class RCache:
-    def __init__(self):
-        from cachetools import TTLCache
-
-        self.c = TTLCache(1024, 60)
-
-    async def aget(self, url, headers=None):
-        self.c.expire()
-        if not (res := self.c.get(url)):
-            res = requests.get(url, headers=headers)
-            self.c[url] = res
-        return res
-
-    def get(self, url, headers=None):
-        self.c.expire()
-        if not (res := self.c.get(url)):
-            res = requests.get(url, headers=headers)
-            self.c[url] = res
-        return res
-
-
-RC = RCache()
-
-
-class GHStore(BaseStore):
-    def _other(self):
-        return lambda p: type(self)(p, self.acc)
-
-    def __init__(self, path, acc=None):
-        if not isinstance(path, Path):
-            path = Path(path)
-        self.path = path
-        self.acc = acc
-
-    def glob(self, arg):
-        data = RC.get(
-            "https://api.github.com/repos/Carreau/papyri-data/git/trees/master",
-            headers={"Authorization": header},
-        ).json()
-        r = gre(arg)
-        res = []
-        for item in data["tree"]:
-            data = RC.get(item["url"], headers={"Authorization": header}).json()
-            res += [
-                self._other()(Path(k)) for x in data["tree"] if r.match(k := x["path"])
-            ]
-
-        return res
-
-    async def read_text(self):
-        data = (
-            await RC.aget(
-                f"https://api.github.com/repos/Carreau/papyri-data/contents/ingest/{str(self.path)}",
-                headers={"Authorization": header},
-            )
-        ).json()
-        raw = await RC.aget(data["download_url"])
-        return raw.text
-
-    async def exists(self):
-        data = (
-            await RC.aget(
-                f"https://api.github.com/repos/Carreau/papyri-data/contents/ingest/{str(self.path)}",
-                headers={"Authorization": header},
-            )
-        ).json()
-        res = "download_url" in data
-        return res
-
-
-class Store(BaseStore):
-    pass
 
 
 class CleanLoader(FileSystemLoader):
---
+++ papyri/stores.py
@@ -0,0 +1,126 @@
+import os
+import re
+from pathlib import Path
+
+import requests
+
+
+class BaseStore:
+    def __init__(self, path):
+        if not isinstance(path, Path):
+            path = Path(path)
+        assert isinstance(path, Path)
+        self.path = path
+
+    def _other(self):
+        return type(self)
+
+    def __truediv__(self, other):
+        return self._other()(self.path / other)
+
+    def __str__(self):
+        return str(self.path)
+
+    async def exists(self):
+        return self.path.exists()
+
+    async def read_text(self):
+        return self.path.read_text()
+
+    def glob(self, arg):
+        return [self._other()(x) for x in self.path.glob(arg)]
+
+    @property
+    def name(self):
+        return self.path.name
+
+    def __lt__(self, other):
+        return self.path < other.path
+
+    def __le__(self, other):
+        return self.path <= other.path
+
+    def __eq__(self, other):
+        return self.path == other.path
+
+
+PAT = os.environ.get("PAT", None)
+header = f"token {PAT}"
+
+
+def gre(pat):
+    return re.compile(pat.replace(".", "\.").replace("*", ".+"))
+
+
+class RCache:
+    def __init__(self):
+        from cachetools import TTLCache
+
+        self.c = TTLCache(1024, 60)
+
+    async def aget(self, url, headers=None):
+        self.c.expire()
+        if not (res := self.c.get(url)):
+            res = requests.get(url, headers=headers)
+            self.c[url] = res
+        return res
+
+    def get(self, url, headers=None):
+        self.c.expire()
+        if not (res := self.c.get(url)):
+            res = requests.get(url, headers=headers)
+            self.c[url] = res
+        return res
+
+
+RC = RCache()
+
+
+class GHStore(BaseStore):
+    def _other(self):
+        return lambda p: type(self)(p, self.acc)
+
+    def __init__(self, path, acc=None):
+        if not isinstance(path, Path):
+            path = Path(path)
+        self.path = path
+        self.acc = acc
+
+    def glob(self, arg):
+        data = RC.get(
+            "https://api.github.com/repos/Carreau/papyri-data/git/trees/master",
+            headers={"Authorization": header},
+        ).json()
+        r = gre(arg)
+        res = []
+        for item in data["tree"]:
+            data = RC.get(item["url"], headers={"Authorization": header}).json()
+            res += [
+                self._other()(Path(k)) for x in data["tree"] if r.match(k := x["path"])
+            ]
+
+        return res
+
+    async def read_text(self):
+        data = (
+            await RC.aget(
+                f"https://api.github.com/repos/Carreau/papyri-data/contents/ingest/{str(self.path)}",
+                headers={"Authorization": header},
+            )
+        ).json()
+        raw = await RC.aget(data["download_url"])
+        return raw.text
+
+    async def exists(self):
+        data = (
+            await RC.aget(
+                f"https://api.github.com/repos/Carreau/papyri-data/contents/ingest/{str(self.path)}",
+                headers={"Authorization": header},
+            )
+        ).json()
+        res = "download_url" in data
+        return res
+
+
+class Store(BaseStore):
+    pass
