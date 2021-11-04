a6d1e810e06c7f40e7cba0a666390ec24977cfb8
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-11-03 21:36:53 -0700

    add errror module

---
+++ papyri/errors.py
@@ -0,0 +1,18 @@
+class VisitTargetNotImplementedError(NotImplementedError):
+    pass
+
+
+class VisitCommentNotImplementedError(NotImplementedError):
+    pass
+
+
+class VisitCitationReferenceNotImplementedError(NotImplementedError):
+    pass
+
+
+class VisitCitationNotImplementedError(NotImplementedError):
+    pass
+
+
+class SerialisationError(Exception):
+    pass
