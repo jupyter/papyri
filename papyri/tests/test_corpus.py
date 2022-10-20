2371bbdd72b3681fff4f6b1951a6803a3b410ecc
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-10-20 11:14:13 +0200

    start to add corpus testing

---
+++ papyri/tests/test_corpus.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pytest
+
+import json
+
+from .utils import _expected_path, SAMPLES, _serialize, _process
+
+PAIRS = [(s, _expected_path(s)) for s in SAMPLES]
+
+
+@pytest.mark.parametrize("sample, expected", PAIRS)
+def test_corpus(sample, expected):
+    processed = _process(sample)
+    exported = _serialize(processed)
+
+    assert expected.read_bytes() == exported
