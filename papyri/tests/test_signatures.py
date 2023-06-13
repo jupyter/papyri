8d68c2160af9a4f6e71de6b03be73a3cdc9fe864
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-06-13 11:31:43 +0200

    add basic tests

--- papyri/tests/test_signatures.py
+++ papyri/tests/test_signatures.py
@@ -2,14 +2,211 @@
 This file is meant to test the serialisation / deserialisation 
 of function signture to JSON.
 """
+from papyri.signature import Signature as SignatureObject
+import json
+
+import pytest
+
+all_funs = []
 
 
+def add(func):
+    global all_funs
+    all_funs.append(func)
+    return func
+
+
+@add
 def function_1(posonly, /, pos_or_k, pos_ok_k_d=1, *varargs, **varkwargs):
+    """
+    {
+      "kind": "function",
+      "parameters": [
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_ONLY",
+          "name": "posonly",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_or_k",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_ok_k_d",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_POSITIONAL",
+          "name": "varargs",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_KEYWORD",
+          "name": "varkwargs",
+          "type": "ParameterNode"
+        }
+      ],
+      "type": "SignatureNode"
+    }"""
     pass
 
 
-def test_f1():
-    from papyri.signature import Signature as SignatureObject
+@add
+def async_function_1(posonly, /, pos_or_k, pos_ok_k_d=1, *varargs, **varkwargs):
+    """{
+      "kind": "function",
+      "parameters": [
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_ONLY",
+          "name": "posonly",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_or_k",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_ok_k_d",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_POSITIONAL",
+          "name": "varargs",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_KEYWORD",
+          "name": "varkwargs",
+          "type": "ParameterNode"
+        }
+      ],
+      "type": "SignatureNode"
+    }"""
+    pass
 
-    so = SignatureObject(function_1)
-    assert so.to_node().to_json() == function_1.__doc__
+
+@add
+def generator_function_1(posonly, /, pos_or_k, pos_ok_k_d=1, *varargs, **varkwargs):
+    """{
+      "kind": "function",
+      "parameters": [
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_ONLY",
+          "name": "posonly",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_or_k",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_ok_k_d",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_POSITIONAL",
+          "name": "varargs",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_KEYWORD",
+          "name": "varkwargs",
+          "type": "ParameterNode"
+        }
+      ],
+      "type": "SignatureNode"
+    }"""
+    yield
+
+
+@add
+async def async_generator_function_1(
+    posonly, /, pos_or_k, pos_ok_k_d=1, *varargs, **varkwargs
+):
+    """{
+      "kind": "function",
+      "parameters": [
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_ONLY",
+          "name": "posonly",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_or_k",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "POSITIONAL_OR_KEYWORD",
+          "name": "pos_ok_k_d",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_POSITIONAL",
+          "name": "varargs",
+          "type": "ParameterNode"
+        },
+        {
+          "annotation": null,
+          "default": null,
+          "kind": "VAR_KEYWORD",
+          "name": "varkwargs",
+          "type": "ParameterNode"
+        }
+      ],
+      "type": "SignatureNode"
+    }"""
+    yield
+
+
+@pytest.mark.parametrize(
+    "func",
+    all_funs,
+)
+def test_f1(func):
+    so = SignatureObject(func)
+    assert json.loads(so.to_node().to_json()) == json.loads(func.__doc__)
