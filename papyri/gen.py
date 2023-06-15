8c63a9ce092242ef51171569bd9b33773c95ff2f
Amit Kumar <dtu.amit@gmail.com>
2023-06-15 12:22:07 +0100

    linting

--- papyri/gen.py
+++ papyri/gen.py
@@ -1802,7 +1802,9 @@ class Gen:
         builtin_function_or_method = type(sum)
 
         if isinstance(target_item, ModuleType):
-            api_object = APIObjectInfo("module", target_item.__doc__, None, target_item.__name__)
+            api_object = APIObjectInfo(
+                "module", target_item.__doc__, None, target_item.__name__
+            )
         elif isinstance(target_item, (FunctionType, builtin_function_or_method)):
             sig: Optional[str]
             try:
@@ -1813,13 +1815,19 @@ class Gen:
             except (ValueError, TypeError):
                 sig = None
             try:
-                api_object = APIObjectInfo("function", target_item.__doc__, sig, target_item.__name__)
+                api_object = APIObjectInfo(
+                    "function", target_item.__doc__, sig, target_item.__name__
+                )
             except Exception as e:
                 raise type(e)(f"For object {qa!r}")
         elif isinstance(target_item, type):
-            api_object = APIObjectInfo("class", target_item.__doc__, None, target_item.__name__)
+            api_object = APIObjectInfo(
+                "class", target_item.__doc__, None, target_item.__name__
+            )
         else:
-            api_object = APIObjectInfo("other", target_item.__doc__, None, target_item.__name__)
+            api_object = APIObjectInfo(
+                "other", target_item.__doc__, None, target_item.__name__
+            )
             # print("Other", target_item)
             # assert False, type(target_item)
 
