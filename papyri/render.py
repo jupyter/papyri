ee1ae897cb5258d88b78cbb7aeedac4dbaef0048
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-09-27 14:18:24 +0200

    update soem typing

--- papyri/render.py
+++ papyri/render.py
@@ -42,7 +42,7 @@ log = logging.getLogger("papyri")
 CSS_DATA = HtmlFormatter(style="pastie").get_style_defs(".highlight")
 
 
-def minify(s):
+def minify(s: str) -> str:
     return minify_html.minify(
         s, minify_js=True, remove_processing_instructions=True, keep_closing_tags=True
     )
@@ -63,7 +63,7 @@ class CleanLoader(FileSystemLoader):
         return until_ruler(source), filename, uptodate
 
 
-def until_ruler(doc):
+def until_ruler(doc: str) -> str:
     """
     Utilities to clean jinja template;
 
@@ -98,9 +98,11 @@ def until_ruler(doc):
 # doable after purely as frontend thing.
 
 
-def compute_siblings_II(ref, family: set):
+def compute_siblings_II(
+    ref: str, family: Set[RefInfo]
+) -> Dict[str, List[Tuple[str, str]]]:
     """ """
-    from collections import defaultdict
+    assert isinstance(ref, str)
 
     module_versions = defaultdict(lambda: set())
     for f in family:
@@ -131,7 +133,7 @@ def compute_siblings_II(ref, family: set):
     return siblings
 
 
-def make_tree(names):
+def make_tree(names: List[str]):
     rd = lambda: defaultdict(rd)
     tree = defaultdict(rd)
 
@@ -1120,7 +1122,7 @@ def old_render_one(
     store: GraphStore,
     template,
     doc: IngestedBlobs,
-    qa,
+    qa: str,
     *,
     current_type,
     backrefs,
