841ac12acb930c043685c2dbce89904a48820199
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-11-20 15:02:56 +0100

    ... to >>>

--- papyri/examples.py
+++ papyri/examples.py
@@ -107,13 +107,13 @@ def example1(
     Examples
     --------
     >>> import matplotlib.pyplot as plt
-    ... fig, ax = plt.subplots()
-    ... ax.plot(range(10))
+    >>> fig, ax = plt.subplots()
+    >>> ax.plot(range(10))
 
     And now
 
     >>> ax.plot(range(5, 15))
-    ... plt.show()
+    >>> plt.show()
 
     """
     return "ok"
@@ -124,24 +124,24 @@ def example2():
     Examples
     --------
     >>> import numpy as np
-    ... import matplotlib.pyplot as plt
-    ... from scipy.interpolate import UnivariateSpline
+    >>> import matplotlib.pyplot as plt
+    >>> from scipy.interpolate import UnivariateSpline
 
     >>> x = np.linspace(-3, 3, 50)
-    ... y = np.exp(-(x ** 2)) + 0.1 * np.random.randn(50)
-    ... plt.plot(x, y, "ro", ms=5)
+    >>> y = np.exp(-(x ** 2)) + 0.1 * np.random.randn(50)
+    >>> plt.plot(x, y, "ro", ms=5)
 
     Use the default value for the smoothing parameter:
 
     >>> spl = UnivariateSpline(x, y)
-    ... xs = np.linspace(-3, 3, 1000)
-    ... plt.plot(xs, spl(xs), "g", lw=3)
+    >>> xs = np.linspace(-3, 3, 1000)
+    >>> plt.plot(xs, spl(xs), "g", lw=3)
 
     Manually change the amount of smoothing:
 
     >>> spl.set_smoothing_factor(0.5)
-    ... plt.plot(xs, spl(xs), "b", lw=3)
-    ... plt.show()
+    >>> plt.plot(xs, spl(xs), "b", lw=3)
+    >>> plt.show()
 
     """
     pass
