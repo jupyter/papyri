04d733a51f91791f0566458f33e79792aed5cfaa
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-11-29 11:47:13 +0100

    add test (and remove a warn/warning deprecation)

--- papyri/tests/test_parse.py
+++ papyri/tests/test_parse.py
@@ -39,6 +39,27 @@ def test_parse_space():
     )
 
 
+def test_parse_no_newline():
+    """
+    Here we test that sections of test that contain new line in the source do
+    not have new line in the output. This make it simpler to render on output
+    that respect newlines
+    """
+    data = dedent(
+        """
+    we want to make sure that `this
+    interpreted_text` not have a newline in it and that `this
+    reference`_ does not either."""
+    ).encode()
+
+    [section] = parse(data, "test_parse_space")
+    text0, directive, text1, reference, text2 = section.children[0].children
+    assert "\n" not in directive.value
+    assert directive.value == "this interpreted_text"
+    assert "\n" not in reference.value
+    assert reference.value == "this reference"
+
+
 def test_parse_reference():
     [section] = parse(
         "This is a `reference <to this>`_".encode(), "test_parse_reference"
